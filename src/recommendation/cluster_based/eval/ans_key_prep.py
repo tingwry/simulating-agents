76f3b1b361575696d14d232dfcc28a100cb5a3ca
tingwry <weeraya972546@gmail.com>
2025-07-29 14:43:55 +0700

    feat: add ndcg binary

--- src/recommendation/cluster_based/eval/ans_key_prep.py
+++ src/recommendation/cluster_based/eval/ans_key_prep.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 
-user_item_matrix = pd.read_csv('src/data/cf_demog_summary/user_item_matrix/user_item_matrix.csv')
+# binary (0/1)
+def binary_ans_key_prep():
+    user_item_matrix = pd.read_csv('src/data/cf_demog_summary/user_item_matrix/user_item_matrix.csv')
 
-user_item_matrix_binary = user_item_matrix.copy()
-user_item_matrix_binary.iloc[:, 1:] = np.where(user_item_matrix.iloc[:, 1:] > 0, 1, 0)
+    user_item_matrix_binary = user_item_matrix.copy()
+    user_item_matrix_binary.iloc[:, 1:] = np.where(user_item_matrix.iloc[:, 1:] > 0, 1, 0)
 
 
-output_path = 'src/recommendation/cluster_based/eval/ans_key.csv'
-user_item_matrix_binary.to_csv(output_path, index=False)
+    output_path = 'src/recommendation/cluster_based/eval/ans_key.csv'
+    user_item_matrix_binary.to_csv(output_path, index=False)
